0316765fc30c2269a9b56a1e6af6a4abea329cfd
vinpel <vinpel@hotmail.com>
2015-01-14 22:38:10 +0100

    0.0.2-RC2 functional install

--- spk/gaspSync/src/dsm-control.sh
+++ spk/gaspSync/src/dsm-control.sh
@@ -13,51 +13,76 @@ ENABLED_FILE="/var/packages/${PACKAGE}/enabled"
 VIRTUALHOST_FILE="/etc/httpd/conf/extra/httpd-${PACKAGE}.conf-user"
 HTTPD_CONFIG_FILE="/etc/httpd/conf/httpd.conf-user"
 
+CFG_FILE="${wizard_gasp_dir}\config\synology.virtualhost.conf"
+
 USER="$([ $(grep buildnumber /etc.defaults/VERSION | cut -d"\"" -f2) -ge 4418 ] && echo -n http || echo -n nobody)"
 
 daemon_status ()
 {
-    if [ -f ${ENABLED_FILE} ]; then
-        return
-    fi
+  if [ -f ${ENABLED_FILE} ]; then
+    return
+  else
     return 1
+  fi
+
 }
 
 
 case $1 in
-    start)
-		sed -i -e "s|Include ${VIRTUALHOST_FILE}||g" ${HTTPD_CONFIG_FILE}
-		echo "Include ${VIRTUALHOST_FILE}" >> ${HTTPD_CONFIG_FILE}
-		if [ "${USER}" == "nobody" ]; then
-			/usr/syno/etc/rc.d/S97apache-user.sh restart
-		else
-			/usr/syno/sbin/synoservicecfg --restart httpd-user
-		fi
-        exit 0
-        ;;
-    stop)
-		sed -i -e "s|Include ${VIRTUALHOST_FILE}||g" ${HTTPD_CONFIG_FILE}
-		if [ "${USER}" == "nobody" ]; then
-			/usr/syno/etc/rc.d/S97apache-user.sh restart
-		else
-			/usr/syno/sbin/synoservicecfg --restart httpd-user
-		fi
-		
-        exit 0
-        ;;
-    status)
-        if daemon_status; then
-            echo ${DNAME} is running
-            exit 0
-        else
-            echo ${DNAME} is not running
-            exit 1
-        fi
-        ;;
-    log)
-        exit 1
-        ;;
-    *)
-        exit 1
-        ;;
+  start)
+  # create the open base dir configuration
+  if [ "${USER}" == "nobody" ]; then
+    echo -e "<Directory \"${wizard_gasp_dir}\">\nphp_admin_value open_basedir \${open_basedir}:${wizard_gasp_dir} \n</Directory>" > /usr/syno/etc/sites-enabled-user/zz_${PACKAGE}.conf
+  else
+
+    echo -e "open_basedir = \${open_basedir}:${wizard_gasp_dir}" > /etc/php/conf.d/zz_${PACKAGE_NAME}.ini
+  fi
+  #copy the virtualhost if not present
+  if [ ! -f ${VIRTUALHOST_FILE} ]; then
+    cp ${CFG_FILE} ${VIRTUALHOST_FILE}
+  fi
+
+  # readd the incluse in the apache config
+  sed -i -e "s|Include ${VIRTUALHOST_FILE}||g" ${HTTPD_CONFIG_FILE}
+  echo "Include ${VIRTUALHOST_FILE}" >> ${HTTPD_CONFIG_FILE}
+
+  #restart the apache user
+  if [ "${USER}" == "nobody" ]; then
+    /usr/syno/etc/rc.d/S97apache-user.sh restart
+  else
+    /usr/syno/sbin/synoservicecfg --restart httpd-user
+  fi
+  exit 0
+  ;;
+
+  
+  stop)
+  # remove the inclusion in apache config file
+  sed -i -e "s|Include ${VIRTUALHOST_FILE}||g" ${HTTPD_CONFIG_FILE}
+  # delete the openbasedir configuration and restart apache user
+  if [ "${USER}" == "nobody" ]; then
+    rm -f /usr/syno/etc/sites-enabled-user/zz_${PACKAGE}.conf
+    /usr/syno/etc/rc.d/S97apache-user.sh restart
+  else
+    rm -f /etc/php/conf.d/zz_${PACKAGE_NAME}.ini
+    /usr/syno/sbin/synoservicecfg --restart httpd-user
+  fi
+
+  exit 0
+  ;;
+  status)
+  if daemon_status; then
+    echo ${DNAME} is running
+    exit 0
+  else
+    echo ${DNAME} is not running
+    exit 1
+  fi
+  ;;
+  log)
+  exit 1
+  ;;
+  *)
+  exit 1
+  ;;
 esac
--- spk/gaspSync/src/installer.sh
+++ spk/gaspSync/src/installer.sh
@@ -8,6 +8,9 @@ PACKAGE_NAME="com.synocommunity.packages.${PACKAGE}"
 # Others
 INSTALL_DIR="/usr/local/${PACKAGE}"
 
+MYSQL="/usr/syno/mysql/bin/mysql"
+MYSQLDUMP="/usr/syno/mysql/bin/mysqldump"
+
 
 WEB_DIR="${wizard_gasp_dir}/web"
 
@@ -25,126 +28,160 @@ VIRTUALHOST_FILE="/etc/httpd/conf/extra/httpd-${PACKAGE}.conf-user"
 #		<VirtualHost *:80>
 #		    Include sites-enabled-user/*.conf
 #		</VirtualHost>
-# cause we use another port. 
+# cause we use another port.
 HTTPD_CONFIG_FILE="/etc/httpd/conf/httpd.conf-user"
 
 USER="$([ $(grep buildnumber /etc.defaults/VERSION | cut -d"\"" -f2) -ge 4418 ] && echo -n http || echo -n nobody)"
 
 preinst ()
 {
-    exit 0
+  # Check database (taken from tt-rss package)
+  if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
+    if ! ${MYSQL} -u root -p"${wizard_mysql_password_root}" -e quit > /dev/null 2>&1; then
+      echo "Incorrect MySQL root password"
+      exit 1
+    fi
+    if ${MYSQL} -u root -p"${wizard_mysql_password_root}" mysql -e "SELECT User FROM user" | grep ^${wizard_gasp_dblogin}$ > /dev/null 2>&1; then
+      echo "MySQL user ${wizard_gasp_dblogin} already exists"
+      exit 1
+    fi
+    if ${MYSQL} -u root -p"${wizard_mysql_password_root}" -e "SHOW DATABASES" | grep ^${wizard_gasp_dbname}$ > /dev/null 2>&1; then
+      echo "MySQL database ${wizard_gasp_dbname} already exists"
+      exit 1
+    fi
+  fi
+  exit 0
 }
 
 
 postinst ()
 {
-    # Link
-    ln -s ${SYNOPKG_PKGDEST} ${INSTALL_DIR}
+  # Link
+  ln -s ${SYNOPKG_PKGDEST} ${INSTALL_DIR}
 
-    # Install the web interface
-    cp -pR ${INSTALL_DIR}/share/${PACKAGE} ${wizard_gasp_dir}
-	
-    if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
-        # Configure open_basedir
-		# the name zz_ is for loading AFTER user-setting cause we touch the global openbasedir
-		# need to be tested for DSM 4.3 ...
-		
-        if [ "${USER}" == "nobody" ]; then
-            echo -e "<Directory \"${wizard_gasp_dir}\">\nphp_admin_value open_basedir \${open_basedir}:${wizard_gasp_dir} \n</Directory>" > /usr/syno/etc/sites-enabled-user/zz_${PACKAGE}.conf
-        else
-			
-            echo -e "open_basedir = \${open_basedir}:${wizard_gasp_dir}" > /etc/php/conf.d/zz_${PACKAGE_NAME}.ini
-        fi
+  # Install the web interface
+  cp -pR ${INSTALL_DIR}/share/${PACKAGE} ${wizard_gasp_dir}
 
+  if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
+    # Configure open_basedir
+    # the name zz_ is for loading AFTER user-setting cause we touch the global openbasedir
+    # need to be tested for DSM 4.3 ...
 
-        # Create a default configuration file init.synology.init.php => local.init.php
-        if [ ! -f ${CFG_FILE} ]; then
-          cp ${DEFAULT_CFG_FILE} ${CFG_FILE}
-		  sed -i -e "s|@wizard_gasp_port@|${wizard_gasp_port:=4000}|g" ${CFG_FILE}
-		  sed -i -e "s|@wizard_calibre_hostname@|${wizard_calibre_hostname:=locahost}|g" ${CFG_FILE}
-		  sed -i -e "s|@wizard_gasp_dbhost@|${wizard_gasp_dbhost}|g" ${CFG_FILE}
-		  sed -i -e "s|@wizard_gasp_dbname@|${wizard_gasp_dbname}|g" ${CFG_FILE}
-		  sed -i -e "s|@wizard_gasp_dblogin@|${wizard_gasp_dblogin}|g" ${CFG_FILE}
-          sed -i -e "s|@wizard_gasp_dbpassword@|${wizard_gasp_dbpassword}|g" ${CFG_FILE}
-          chmod ga+w ${CFG_FILE}
-        fi
-		echo "DEFAULT_VIRTUALHOST_FILE" > /volume1/monbootstrap/test.tst
-		#  copy and edit the virtual-host configuration
-		if [ ! -f ${VIRTUALHOST_FILE} ]; then
-          cp ${DEFAULT_VIRTUALHOST_FILE} ${VIRTUALHOST_FILE}
-		  sed -i -e "s|@wizard_gasp_port@|${wizard_gasp_port:=}|g" ${VIRTUALHOST_FILE}
-		  sed -i -e "s|@wizard_calibre_hostname@|${wizard_calibre_hostname:=}|g" ${VIRTUALHOST_FILE}
-		  # the public directory is the Web directory (warning name are different)
-		  sed -i -e "s|@wizard_gasp_dir@|${WEB_DIR}|g" ${VIRTUALHOST_FILE}
-          chmod ga+w ${VIRTUALHOST_FILE}
-        fi
-		# Add the virtual host to the Apache user config
-		
-			# if the user check "launch after install" it will launch, no need to strange behaviour
-		# remove if already installed
-		#sed -i -e "s|Include ${VIRTUALHOST_FILE}||g" ${HTTPD_CONFIG_FILE}
-		# add / readd the inclusion 
-		#echo "Include ${VIRTUALHOST_FILE}" >> ${HTTPD_CONFIG_FILE}
-		
-		
-		
-		sed -i -e "s|Include /etc/httpd/conf/extra/httpd-gaspSync.conf-user||g"
-		
+    if [ "${USER}" == "nobody" ]; then
+      echo -e "<Directory \"${wizard_gasp_dir}\">\nphp_admin_value open_basedir \${open_basedir}:${wizard_gasp_dir} \n</Directory>" > /usr/syno/etc/sites-enabled-user/zz_${PACKAGE}.conf
+    else
 
-		# perform install
-		${wizard_gasp_dir}/yii install/synology
-		# create database
-		# ${wizard_gasp_dir}/yii migrate --interactive=0 
-		
-		# Set permissions
-        chown ${USER} ${wizard_gasp_dir} -R
+      echo -e "open_basedir = \${open_basedir}:${wizard_gasp_dir}" > /etc/php/conf.d/zz_${PACKAGE_NAME}.ini
     fi
 
-    exit 0
+
+    # Create a default configuration file init.synology.init.php => local.init.php
+    if [ ! -f ${CFG_FILE} ]; then
+      cp ${DEFAULT_CFG_FILE} ${CFG_FILE}
+      sed -i -e "s|@wizard_gasp_port@|${wizard_gasp_port:=4000}|g" ${CFG_FILE}
+      sed -i -e "s|@wizard_calibre_hostname@|${wizard_calibre_hostname:=locahost}|g" ${CFG_FILE}
+      sed -i -e "s|@wizard_gasp_dbhost@|${wizard_gasp_dbhost}|g" ${CFG_FILE}
+      sed -i -e "s|@wizard_gasp_dbname@|${wizard_gasp_dbname}|g" ${CFG_FILE}
+      sed -i -e "s|@wizard_gasp_dblogin@|${wizard_gasp_dblogin}|g" ${CFG_FILE}
+      sed -i -e "s|@wizard_gasp_dbpassword@|${wizard_gasp_dbpassword}|g" ${CFG_FILE}
+      chmod ga+w ${CFG_FILE}
+      # copy the config file in case of update of DSM to be able to replace it.
+      cp ${CFG_FILE} "${wizard_gasp_dir}\config\synology.virtualhost.conf"
+    fi
+    echo "DEFAULT_VIRTUALHOST_FILE" > /volume1/monbootstrap/test.tst
+    #  copy and edit the virtual-host configuration
+    if [ ! -f ${VIRTUALHOST_FILE} ]; then
+      cp ${DEFAULT_VIRTUALHOST_FILE} ${VIRTUALHOST_FILE}
+      sed -i -e "s|@wizard_gasp_port@|${wizard_gasp_port:=}|g" ${VIRTUALHOST_FILE}
+      sed -i -e "s|@wizard_calibre_hostname@|${wizard_calibre_hostname:=}|g" ${VIRTUALHOST_FILE}
+      # the public directory is the Web directory (warning name are different)
+      sed -i -e "s|@wizard_gasp_dir@|${WEB_DIR}|g" ${VIRTUALHOST_FILE}
+      chmod ga+w ${VIRTUALHOST_FILE}
+    fi
+    # create the database
+    if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
+      ${MYSQL} -u root -p"${wizard_mysql_password_root}" -e "CREATE DATABASE ${wizard_gasp_dbname}; GRANT ALL PRIVILEGES ON ${wizard_gasp_dbname}.* TO '${wizard_gasp_dblogin}'@'localhost' IDENTIFIED BY '${wizard_gasp_dbpassword}';"
+    fi
+
+    # perform the install
+    ${wizard_gasp_dir}/yii install/synology
+    # create the database
+     ${wizard_gasp_dir}/yii migrate --interactive=0
+
+    # Set permissions
+    chown ${USER} ${wizard_gasp_dir} -R
+  fi
+
+  exit 0
 }
 
 preuninst ()
 {
-    exit 0
+  # Check database
+  #if [ "${SYNOPKG_PKG_STATUS}" == "UNINSTALL" ] && ! ${MYSQL} -u root -p"${wizard_mysql_password_root}" -e quit > /dev/null 2>&1; then
+  #    echo "Incorrect MySQL root password"
+  #    exit 1
+  #fi
+
+  # Check database export location
+  #if [ "${SYNOPKG_PKG_STATUS}" == "UNINSTALL" -a -n "${wizard_dbexport_path}" ]; then
+  #    if [ -f "${wizard_dbexport_path}" -o -e "${wizard_dbexport_path}/${MYSQL_DATABASE}.sql" ]; then
+  #      echo "File ${wizard_dbexport_path}/${MYSQL_DATABASE}.sql already exists. Please remove or choose a different location"
+  #      exit 1
+  #    fi
+  #fi
+  exit 0
 }
 
 postuninst ()
 {
-    # Remove install dir
-    rm -Rf ${wizard_gasp_dir}
+  # Remove install dir
+  rm -fr ${wizard_gasp_dir}
+  #echo "rm -fr ${wizard_gasp_dir}">>/volume1/test.txt
 
-    # Remove open_basedir configuration
-    rm -f /usr/syno/etc/sites-enabled-user/zz_${PACKAGE}.conf
-    rm -f /etc/php/conf.d/zz_${PACKAGE_NAME}.ini
-	
-	# remove virtualhost configuration
-	rm -f ${VIRTUALHOST_FILE}
-	sed -i -e "s|Include ${VIRTUALHOST_FILE}||g" ${HTTPD_CONFIG_FILE}
-    exit 0
+  # Remove open_basedir configuration
+  rm -f /usr/syno/etc/sites-enabled-user/zz_${PACKAGE}.conf
+  rm -f /etc/php/conf.d/zz_${PACKAGE_NAME}.ini
+
+  # remove virtualhost configuration
+  rm -f ${VIRTUALHOST_FILE}
+  # remove the inclusion
+  sed -i -e "s|Include ${VIRTUALHOST_FILE}||g" ${HTTPD_CONFIG_FILE}
+
+  # Export and remove database
+  #if [ "${SYNOPKG_PKG_STATUS}" == "UNINSTALL" ]; then
+  #    if [ -n "${wizard_dbexport_path}" ]; then
+  #      mkdir -p ${wizard_dbexport_path}
+  #      ${MYSQLDUMP} -u root -p"${wizard_mysql_password_root}" ${MYSQL_DATABASE} > ${wizard_dbexport_path}/${MYSQL_DATABASE}.sql
+  #    fi
+  #    ${MYSQL} -u root -p"${wizard_mysql_password_root}" -e "DROP DATABASE ${MYSQL_DATABASE}; DROP USER '${MYSQL_USER}'@'localhost';"
+  #fi
+
+  exit 0
 }
 
 preupgrade ()
 {
-    # Save some stuff
-    #rm -fr ${TMP_DIR}/${PACKAGE}
-#    mkdir -p ${TMP_DIR}/${PACKAGE}
-    #mv ${CFG_FILE} ${TMP_DIR}/${PACKAGE}/
-    #if [ "${USER}" == "nobody" ]; then
-    #    mv /usr/syno/etc/sites-enabled-user/${PACKAGE}.conf ${TMP_DIR}/${PACKAGE}/
-    #else
-        #mv /etc/php/conf.d/${PACKAGE_NAME}.ini ${TMP_DIR}/${PACKAGE}/
-    #fi
-    exit 0
+  # Save some stuff
+  #rm -fr ${TMP_DIR}/${PACKAGE}
+  #    mkdir -p ${TMP_DIR}/${PACKAGE}
+  #mv ${CFG_FILE} ${TMP_DIR}/${PACKAGE}/
+  #if [ "${USER}" == "nobody" ]; then
+  #    mv /usr/syno/etc/sites-enabled-user/${PACKAGE}.conf ${TMP_DIR}/${PACKAGE}/
+  #else
+  #mv /etc/php/conf.d/${PACKAGE_NAME}.ini ${TMP_DIR}/${PACKAGE}/
+  #fi
+  exit 1
 }
 
 postupgrade ()
 {
-    # Restore some stuff
-    #rm -f ${CFG_FILE}
-    #mv ${TMP_DIR}/${PACKAGE}/config_local.php ${CFG_FILE}
-    #mv ${TMP_DIR}/${PACKAGE}/${PACKAGE}.conf /usr/syno/etc/sites-enabled-user
-    ##mv ${TMP_DIR}/${PACKAGE}/${PACKAGE_NAME}.ini /etc/php/conf.d/
-    #rm -fr ${TMP_DIR}/${PACKAGE}
+  # Restore some stuff
+  #rm -f ${CFG_FILE}
+  #mv ${TMP_DIR}/${PACKAGE}/config_local.php ${CFG_FILE}
+  #mv ${TMP_DIR}/${PACKAGE}/${PACKAGE}.conf /usr/syno/etc/sites-enabled-user
+  ##mv ${TMP_DIR}/${PACKAGE}/${PACKAGE_NAME}.ini /etc/php/conf.d/
+  #rm -fr ${TMP_DIR}/${PACKAGE}
 
-    exit 0
+  exit 1
 }
